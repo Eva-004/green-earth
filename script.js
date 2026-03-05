899910ad4394ae0d2d675f893fd7fcc244818c5e
Eva-004 <foredajannateva@gmail.com>
2026-03-05 20:36:43 +0600

    Loading trees data from API

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
   
   const categoriesContainer = document.getElementById('categories-container');
+  const treeContainer = document.getElementById('trees-container');
   async function loadCategoriesBtn() {
     const res= await fetch('https://openapi.programming-hero.com/api/categories');
     const data = await res.json();
@@ -11,4 +12,35 @@
     });
   }
 
+  async function loadTrees() {
+    const res=await fetch('https://openapi.programming-hero.com/api/plants');
+    const data = await res.json();
+     displayTrees(data);
+  }
+ 
+  const displayTrees=(data)=>{
+    data.plants.forEach(tree=>{
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <div class="card bg-base-100  shadow-sm">
+                            <figure>
+                                <img src="${tree.image}"
+                                    alt="${tree.name}" title="${tree.name}" class="h-[30vh] object-cover w-full" />
+                            </figure>
+                            <div class="card-body">
+                                <h2 class="card-title">${tree.name}</h2>
+                                <p class="line-clamp-2">${tree.description}</p>
+                                <div class="badge badge-outline badge-success">${tree.category}</div>
+                                <div class="flex gap-4 justify-between items-center">
+                                    <h2 class=" text-xl text-[#416d51] font-bold">$5000</h2>
+                                    <button class="btn  btn-success text-white ">Cart</button>
+                                </div>
+                            </div>
+                        </div>
+        `
+        treeContainer.appendChild(div);
+    })
+  }
+
   loadCategoriesBtn();
+  loadTrees();
