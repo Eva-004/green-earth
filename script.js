026a022191322d510a9fd12a7f7aa81067cc6dfd
Eva-004 <foredajannateva@gmail.com>
2026-03-06 15:48:29 +0600

    Modal for display tree details dynamically by API

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
   const allTreesBtn = document.getElementById('allTreesBtn');
   function clickAllBtn(id){
     showCategoryTrees(id,allTreesBtn);
+    loadTrees();
   }
   const categoriesContainer = document.getElementById('categories-container');
   const treeContainer = document.getElementById('trees-container');
   const loadingCategories = document.getElementById('loadingCategories');
+  const detailsModal = document.getElementById('details-modal');
+  const detailsImage = document.getElementById('details-image') ;
+  const detailsTitle = document.getElementById('details-title') ;
+  const detailsBadge = document.getElementById('details-badge') ;
+  const detailsDescription = document.getElementById('details-description') ;
+  const detailsPrice= document.getElementById('details-price') ;
   async function loadCategoriesBtn() {
     const res= await fetch('https://openapi.programming-hero.com/api/categories');
     const data = await res.json();
@@ -42,7 +49,7 @@
      const res = await fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`);
      const data = await res.json();
       hideLoading();
-     displayTrees(data.plants);
+     displayTrees(data);
     
   }
 
@@ -51,16 +58,16 @@
     const res=await fetch('https://openapi.programming-hero.com/api/plants');
     const data = await res.json();
     hideLoading();
-     displayTrees(data.plants);
+     displayTrees(data);
   }
  
   const displayTrees=(data)=>{
-    data.forEach(tree=>{
+    data.plants.forEach(tree=>{
         const div = document.createElement('div');
         div.innerHTML=`
         <div class="card bg-base-100  shadow-sm">
-                            <figure>
-                                <img src="${tree.image}"
+                            <figure >
+                                <img onclick="loadDetails(${tree.id})"src="${tree.image}"
                                     alt="${tree.name}" title="${tree.name}" class="h-[30vh] object-cover w-full" />
                             </figure>
                             <div class="card-body">
@@ -72,10 +79,28 @@
                                     <button class="btn  btn-success text-white ">Cart</button>
                                 </div>
                             </div>
-                        </div>
+         </div>
         `
         treeContainer.appendChild(div);
     })
+  }
+
+
+  async function loadDetails(id) {
+    const res = await fetch(`https://openapi.programming-hero.com/api/plant/${id}`);
+    const data = await res.json();
+    const tree = data.plants;
+    detailsTitle.innerText=`${tree.name}`
+    detailsImage.src=`${tree.image}`;
+    detailsBadge.innerText=`${tree.category}`;
+    detailsDescription.innerText=`${tree.description}`;
+    detailsPrice.innerText=`$${tree.price}`;
+    detailsModal.showModal();
+    
+  }
+
+  function close(){
+   detailsModal.classList.add('hidden');
   }
 
   loadCategoriesBtn();
